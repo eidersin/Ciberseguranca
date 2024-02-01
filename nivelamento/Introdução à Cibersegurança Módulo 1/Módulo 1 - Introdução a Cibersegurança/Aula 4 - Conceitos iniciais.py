701f7c3008b2a6a9eeb587781675f1d3dd27e23b
eidersin <dersonts@gmail.com>
2024-02-01 17:26:03 -0300

    Aula 4

---
+++ nivelamento/Introdução à Cibersegurança Módulo 1/Módulo 1 - Introdução a Cibersegurança/Aula 4 - Conceitos iniciais.py
@@ -0,0 +1,31 @@
+# Vírus
+"""
+Vírus:
+    Um vírus de computador é um programa nocivo que se replica em sistemas e arquivos, podendo prejudicar o
+    funcionamento do computador ou roubar dados confidenciais.
+
+    Conceito:
+    Vírus de computador são programas maliciosos que se disseminam por meio de anexos de e-mail, downloads da internet
+    ou dispositivos removíveis, apresentando diversas características e causando diferentes tipos de danos.
+
+    - Vírus de arquivo:
+        Anexa-se a arquivos executáveis e se espalha quando esses arquivos são executados.
+    - Vírus de macro:
+        Explora macros em aplicativos como processadores de texto e planilhas para se espalhar.
+    - Vírus de boot:
+        Infecta a área de inicialização de um disco rígido, tornando-se ativo quando o computador é inicializado.
+    - Worms:
+        Diferente de vírus, os worms não precisam se anexar a arquivos e podem se espalhar automaticamente pela rede.
+    - Trojans:
+        Disfarçados como software legítimo, os cavalos de Troia enganam os usuários para que os executem, permitindo o
+        acesso não autorizado ao sistema.
+
+    Confira na lista abaixo algumas dessa ameaças que podem afetar a segurança e privacidade dos dados.
+
+    - SPAM
+    - Spyware
+    - Worms
+    - Phishing
+    - Botnets
+    - Ransomware
+"""
