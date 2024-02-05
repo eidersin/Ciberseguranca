8e9fb2eb7f54c6c485d4bd77583f2e09bf80c8b9
eidersin <dersonts@gmail.com>
2024-02-05 18:38:12 -0300

    Create: Novas aulas

---
+++ nivelamento/02 - Identificar componentes de hardware de computador/Módulo 2 - Identificar componentes de hardware de computador Ir para o conteúdo principal/Aula 2 - CPU, Placa-mãe, Memórias e BIOS UEFI.py
@@ -0,0 +1,32 @@
+"""
+Rapidamente vamos conhecer os componentes de um computador:
+
+        - Processador
+        - Placa-Mãe
+        - Cooler
+        - Memória RAM
+        - HDD/SSD
+        - Fonte
+        - Placas Offboard
+
+    - Placa mãe
+    A placa mãe é uma peça fundamental em uma máquina e é responsável por facilitar a comunicação entre os diferentes
+    componentes de um computador. Ela é composta por diferentes partes que, em conjunto, trabalham na integração e
+    coordenação de hardware, possibilitando o funcionamento harmonioso do sistema como um todo.
+
+    - Memórias
+
+    A memória de um computador é responsável por armazenar os dados de um sistema. Além disso, ela tem papel crucial quanto
+    a velocidade de utilização e troca de dados de um computador.
+
+    As memórias podem ser separadas em dois tipos:
+        Não Voláteis :
+        ROM (PROMs, EPROMs, EEPROMs, Memória flash)
+        Armazenamento em massa (Pendrive, SSD, HD)
+
+        Voláteis:
+        RAM (SIMM, DIMM, SDR, DDR, DDR2, DDR3, DDR4, DDR5, etc)
+        Cache (L1, L2, L3)
+        Vídeo (GDDR3, GDDR4, GDDR5, GDDR6, HBM, HBM2)
+
+"""
