32fceddde15337d90bf3943133d93739fade509e
eidersin <dersonts@gmail.com>
2024-02-08 19:15:39 -0300

    Create: novas aulas

---
+++ nivelamento/03 - Compreender Internet e Camada de acesso à rede/Módulo 3 - Compreender Internet e Camada de acesso a rede/Aula 6 - Conectividade.py
@@ -0,0 +1,31 @@
+"""
+Conforme a evolução da tecnologia de comunicação, alguns aspectos que era utilizado na era da comunicação analógica
+foram substituídos na era da comunicação digital. Enquanto na analógica o sistema era mais simples, representado por
+informações de maneira contínua e variável na era digital a comunicação passou a ser representada de maneira binária,
+convertidas por sequência digitais para serem utilizadas nos computadores.
+
+    - Transmissão de dados:
+        A transmissão de dados é um processo que envolve a transferência de bits de informação de um ponto a outro, seja
+        local ou global, e é vital para a conectividade de múltiplos aparelhos e dispositivos que utilizamos hoje em dia
+        Essa transmissão pode ser dar por meio de cabos de fibra óptica, de cobre, rede sem sim e seguir uma série de
+        protocolos de transmissão.
+
+    - Comunicação ponto a ponto.
+        Quando no processo de comunicação existem duas antenas que fazem a comunicação direta entre si, chamamos esse
+        processo de comunicação ponto a ponto. Este modelo de transmissão de dados envolve a conexão direta entre dois
+        pontos, seja entre dispositivos, sistemas ou redes. Esse tipo de enlace é essencial em diversas aplicações,
+        proporcionando eficiência, simplicidade e, muitas vezes, maior segurança nas trocas de informações.
+
+    - Portas Lógicas:
+        As portas lógicas são blocos ou circuitos que operam em dispositivos de um ou mais sinais lógicos de entrada
+        produzindo somente uma saída. Algumas nomenclaturas conhecidas de portas lógicas são:
+
+            AND
+            OR
+            NOT
+            NAND
+            NOR
+            XOR
+            XNOR
+
+"""
---
+++ nivelamento/03 - Compreender Internet e Camada de acesso à rede/Módulo 3 - Compreender Internet e Camada de acesso a rede/Aula 7 - Par metálico.py
@@ -0,0 +1,3 @@
+"""
+Os cabos de rede são equipamentos ainda muito importante e utilizados no processo de comunicação e de transmissão de dados em rede. Os cabos de par metálico representam uma das tecnologias mais antigas e são compostos por pares de fios de cobre entrelaçados, esses cabos desempenham um papel vital em redes locais 
+"""
